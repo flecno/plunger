7c77e49a0ac1279187c44fc256fb0e02bb11649c
Timo Zingel <timo.zingel@joblift.de>
2021-03-08 10:47:44 +0100

    test with buffered output

--- plunger/src/main/java/de/galan/plunger/util/Output.java
+++ plunger/src/main/java/de/galan/plunger/util/Output.java
@@ -2,17 +2,30 @@ package de.galan.plunger.util;
 
 import static org.fusesource.jansi.Ansi.*;
 
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+
 import org.fusesource.jansi.Ansi.Color;
 
 
 /**
  * Prints output to the stdout/err, colors can be used via AnsiConsole
- * 
+ *
  * @author daniel
  */
 public class Output {
 
 	private static boolean colors = true;
+	private static PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(new FileOutputStream(java.io.FileDescriptor.out),
+	StandardCharsets.UTF_8), 4096));
+	// 10k local
+	// with sout: 340s
+	// with buffer: 12s
+	// private static PrintStream out = System.out;
 
 
 	public static void setColor(boolean colors) {
@@ -21,22 +34,24 @@ public class Output {
 
 
 	public static void print(Color color, String text) {
-		System./**/out.print(colors ? ansi().fg(color).a(text).reset() : text);
+		out.print(colors ? ansi().fg(color).a(text).reset() : text);
+		// out.print(text);
 	}
 
 
 	public static void println(Color color, String text) {
-		System./**/out.println(colors ? ansi().fg(color).a(text).reset() : text);
+		out.println(colors ? ansi().fg(color).a(text).reset() : text);
+		// out.println(text);
 	}
 
 
 	public static void print(String line) {
-		System/**/.out.print(line);
+		out.print(line);
 	}
 
 
 	public static void println(String line) {
-		System/**/.out.println(line);
+		out.println(line);
 	}
 
 
@@ -44,4 +59,8 @@ public class Output {
 		System./**/err.println(colors ? ansi().fg(Color.RED).a(text).reset() : text);
 	}
 
+
+	public static void flush() {
+		out.flush();
+	}
 }
